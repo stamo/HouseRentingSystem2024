716447eac2e7397a98f71b81c839b86140f616bb
Стамо Петков <s.g.petkov@is-bg.net>
2024-03-01 19:19:06 +0200

    Repository added

---
+++ HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+    }
+}
---
+++ HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(HouseRentingDbContext _context) 
+        {
+            context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+    }
+}
