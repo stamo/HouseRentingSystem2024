7a4d7f8efb9efd3e2f0259ea0ac49abfadeaf8c1
Стамо Петков <s.g.petkov@is-bg.net>
2024-03-01 20:58:33 +0200

    Become an agent implemented

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -5,5 +5,9 @@
         public const string RequiredMessage = "The {0} field is required";
 
         public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone number already exists. Enter another one";
+
+        public const string HasRents = "You should have no rents to become an agent";
     }
 }
--- HouseRentingSystem/Controllers/AgentController.cs
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -1,7 +1,9 @@
-﻿using HouseRentingSystem.Core.Contracts;
+﻿using HouseRentingSystem.Attributes;
+using HouseRentingSystem.Core.Contracts;
 using HouseRentingSystem.Core.Models.Agent;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
 
 namespace HouseRentingSystem.Controllers
 {
@@ -15,21 +17,35 @@ namespace HouseRentingSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Become()
+        [NotAnAgent]
+        public IActionResult Become()
         {
-            if (await agentService.ExistsByIdAsync(User.Id()))
-            {
-                return BadRequest();
-            }
-
             var model = new BecomeAgentFormModel();
 
             return View(model);
         }
 
         [HttpPost]
+        [NotAnAgent]
         public async Task<IActionResult> Become(BecomeAgentFormModel model)
         {
+            if (await agentService.UserWithPhoneNumberExistsAsync(User.Id())) 
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExists);
+            }
+
+            if (await agentService.UserHasRentsAsync(User.Id()))
+            {
+                ModelState.AddModelError("Error", HasRents);
+            }
+
+            if (ModelState.IsValid == false) 
+            {
+                return View(model);
+            }
+
+            await agentService.CreateAsync(User.Id(), model.PhoneNumber);
+
             return RedirectToAction(nameof(HouseController.All), "House");
         }
     }
