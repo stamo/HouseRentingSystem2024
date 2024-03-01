846772526dce2a85784b6b396da10826804c1ec6
Стамо Петков <s.g.petkov@is-bg.net>
2024-03-01 20:00:25 +0200

    Add and SaveChanges implemented in Repository

--- HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
+++ HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
@@ -5,5 +5,9 @@
         IQueryable<T> All<T>() where T : class;
 
         IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
     }
 }
--- HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
+++ HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -26,5 +26,15 @@ namespace HouseRentingSystem.Infrastructure.Data.Common
             return DbSet<T>()
                 .AsNoTracking();
         }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
     }
 }
