0f2201c0f9407ba4e1a8bce60d39c04ab184f6c9
Стамо Петков <s.g.petkov@is-bg.net>
2024-03-01 19:34:35 +0200

    Agent Service added

---
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -0,0 +1,13 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+    }
+}
--- HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -12,6 +12,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IHouseService, HouseService>();
+            services.AddScoped<IAgentService, AgentService>();
 
             return services;
         }
