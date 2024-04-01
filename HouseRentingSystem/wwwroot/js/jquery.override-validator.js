eef2a83f903036ca360c6794268e27cf6454e955
Стамо Петков <s.g.petkov@is-bg.net>
2024-04-01 19:45:02 +0300

    JQuery validator override

---
+++ HouseRentingSystem/wwwroot/js/jquery.override-validator.js
@@ -0,0 +1,9 @@
+﻿jQuery(function ($) {
+    $.validator.addMethod('number', function (value, element) {
+        return this.optional(element) || /^-?(?:\d+)(?:(\.|,)\d+)?$/.test(value);
+    });
+
+    $.validator.methods.range = function (value, element, param) {
+        return this.optional(element) || (Number(value.replace(',', '.')) >= Number(param[0]) && Number(value.replace(',', '.')) <= Number(param[1]));
+    }
+});
