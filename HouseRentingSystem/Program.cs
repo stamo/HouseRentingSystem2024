13d2e7e5f56b8a246f4b71ef7a6482e924bc9b62
Стамо Петков <s.g.petkov@is-bg.net>
2024-04-01 19:44:41 +0300

    Login and Register fixed

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,5 +1,8 @@
 using HouseRentingSystem.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HouseRentingSystem.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
