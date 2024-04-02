71501a23af2a931a2977b08b4e9bc3cf20eaa7b8
Стамо Петков <s.g.petkov@is-bg.net>
2024-04-02 12:10:55 +0300

    Require unique email

--- HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -37,6 +37,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services
                 .AddDefaultIdentity<ApplicationUser>(options =>
                 {
+                    options.User.RequireUniqueEmail = true;
                     options.SignIn.RequireConfirmedAccount = false;
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireDigit = false;
